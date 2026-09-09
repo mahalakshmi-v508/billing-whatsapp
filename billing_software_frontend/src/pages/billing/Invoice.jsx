62b7315fa4eaef7f3334f19754b58fdbfb69b315
manju9711 <manjuterasha97@gmail.com>
2026-09-09 11:24:01 +0530

    invoice bill  ui changes

--- billing_software_frontend/src/pages/billing/Invoice.jsx
+++ billing_software_frontend/src/pages/billing/Invoice.jsx
@@ -1,3 +1,4 @@
+
 import { useEffect, useState, useRef } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import api from "../../services/api";
--- billing_software_frontend/src/pages/settings/InvoiceDesign.jsx
+++ billing_software_frontend/src/pages/settings/InvoiceDesign.jsx
@@ -649,3 +649,4 @@ export default function InvoiceDesign() {
     </SettingsShell>
   );
 }
+  
